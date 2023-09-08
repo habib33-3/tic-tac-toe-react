50c8ac2a2a958e9b9f760d0777ea837f41fc09ab
Habibur Rahman <habib.swi16@gmail.com>
2023-09-08 17:15:53 +0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import "./App.css";
 
-function App() {
+function Square({ value }) {
+  const handleClick = () => {
+    console.log("click");
+  };
+
   return (
     <>
-      <h1>hello</h1>
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
     </>
   );
 }
 
-export default App;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value={"1"} />
+        <Square value={"2"} />
+        <Square value={"3"} />
+      </div>
+
+      <div className="board-row">
+        <Square value={"4"} />
+        <Square value={"5"} />
+        <Square value={"6"} />
+      </div>
+
+      <div className="board-row">
+        <Square value={"7"} />
+        <Square value={"8"} />
+        <Square value={"9"} />
+      </div>
+    </>
+  );
+}
